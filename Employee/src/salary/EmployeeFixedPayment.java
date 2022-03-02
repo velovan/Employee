b9f42e8cbb424ec7fd054d5cc88f686ada945437
velovan <velovan@users.noreply.github.com>
2022-03-02 15:18:56 +0200

    Update EmployeeFixedPayment.java

--- Employee/src/salary/EmployeeFixedPayment.java
+++ Employee/src/salary/EmployeeFixedPayment.java
@@ -5,23 +5,22 @@ public class EmployeeFixedPayment extends Employee {
 	protected double fixedPayment;
 
 	public EmployeeFixedPayment(String firstName, String lastName, int id, double salary) {
-		super(firstName, lastName, id);
-		this.fixedPayment = salary;
+	     super(firstName, lastName, id);
+	     this.fixedPayment = salary;
 	}
 
 	public double getSalary() {
-		return fixedPayment;
+             return fixedPayment;
 	}
 
 	public void setSalary(double salary) {
-		this.fixedPayment = salary;
+	     this.fixedPayment = salary;
 	}
 
 	@Override
 	public double getMonthlySalary() {
-		// TODO Auto-generated method stub
-		
-		return fixedPayment;
+	     // TODO Auto-generated method stub
+	     return fixedPayment;
 	}
 
 }
