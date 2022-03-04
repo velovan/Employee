012fc9582805f456d71b065e8d02d1745a8ff4a0
velovan <velovan@users.noreply.github.com>
2022-03-04 13:04:16 +0200

    Update EmployeeHourlyWage.java

--- Employee/src/salary/EmployeeHourlyWage.java
+++ Employee/src/salary/EmployeeHourlyWage.java
@@ -5,20 +5,20 @@ public class EmployeeHourlyWage extends Employee {
 	protected double salaryWage;
 
 	public EmployeeHourlyWage(String firstName, String lastName, int id, double salaryRate) {
-		super(firstName, lastName, id);
-		this.salaryWage = 20.8 * 8 * salaryRate;
+	       super(firstName, lastName, id);
+	       this.salaryWage = 20.8 * 8 * salaryRate;
 	}
 
 	public double getSalaryWage() {
-		return salaryWage;
+	       return salaryWage;
 	}
 
 	public void setSalaryRate(double salaryRate) {
-		this.salaryWage = 20.8 * 8 * salaryRate;
+	       this.salaryWage = 20.8 * 8 * salaryRate;
 	}
 
 	@Override
 	public double getMonthlySalary() {
-		return salaryWage * 20.8 * 8;
+	       return salaryWage * 20.8 * 8;
 	}
 }
